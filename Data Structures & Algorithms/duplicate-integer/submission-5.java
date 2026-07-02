b1535d872f7b984b9f0ed132991ff3724c5cde62
Adnan Farooq <adnanar0007@gmail.com>
2026-07-02 20:24:16 +0500

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        // 1. Brute Force:
+
+        // for(int i = 0; i < nums.length - 1; i++ ){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i] == nums[j] ){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+
+        // 2. A little better Approach:
+
+        // Arrays.sort(nums);
+        // for(int i = 0; i < nums.length - 1;  i++){
+        //     if(nums[i] == nums[i + 1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+        // 3. Optimised Approach 
+
+        Set<Integer> hashmap = new HashSet<>();
+
+        for(int i = 0; i < nums.length ; i++){
+            if(hashmap.contains(nums[i])){
+                return true;
+            }
+            hashmap.add(nums[i]);
+        }
+        return false;
+
+
+    }
+}
